992d213d438836f8959277b2c35e49924a28a9a1
ABiviano13 <abiviano0@outlook.com>
2023-02-27 15:26:34 +0100

    FEAT generator Email

--- js/app.js
+++ js/app.js
@@ -3,10 +3,17 @@ const { createApp } = Vue;
 createApp({
     data() {
         return{
-
+            emails: []
         }
     },
-    methods : {
-
+    methods: {
+        generatorEmails() {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((res) => {
+                console.log(res);
+                console.log(res.data.response);
+                this.emails.push(res.data.response);
+            })
+        }
     },
 }).mount('#app')
